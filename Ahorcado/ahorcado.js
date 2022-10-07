d6aa1e1c512013b4cb555b285ef4ce4748665291
alberola <93872954+alberola@users.noreply.github.com>
2022-10-07 16:12:55 +0200

    Ahorcado Finalizado

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,11 +1,13 @@
 //Variable que ejecuta la función contador cada 1 seg.
 var cont = setInterval(contador, 1000);
 //Variables de la aplicación
-let num = 60;
+let num = 59;
 let posibilidades = ["COCHE", "MOTO", "GUAGUA", "BICICLETA", "MOTOCICLETA", "PATINETA"];
+let caracteres = [];
 
 let errores = 5;
 let aciertos = 0;
+let contadorCaracteres = 0;
 
 let random = parseInt(Math.random()*6);
 let auxVar = posibilidades[random];
@@ -13,7 +15,7 @@ let auxVar = posibilidades[random];
 //Impresión en consola para facilitar el acierto o fallo de las distintas palabras (para pruebas).
 console.log(auxVar);
 
-//Función que implementa un contador cada.
+//Función que implementa un contador cada 1 seg.
 function contador(){
     if(num == 0){   
         alert("Se ha agotado el tiempo...");    
@@ -39,7 +41,7 @@ function resolver(){
         document.getElementById("espacios").innerHTML = posibilidades[random];
     }
     alert("No seas tramposo he intentalo por ti mismo...");
-    recargar();
+    setTimeout(recargar, 2000);
 }
 //Funcion que implementa el método para comprobar si el caracter introducido es válido
 function probar(){
@@ -48,13 +50,14 @@ function probar(){
     let valorInput = document.getElementById("texto").value.toUpperCase();
     //Variable booleana que indicará cuando no se encuentre caracter.
     let booleana = false;
-
-    for (let index = 0; index < auxVar.length; index++) {
-        if (valorInput == auxVar[index]){
-            booleana = true;
-            aciertos++;
-            document.getElementById("span"+index).innerHTML = valorInput;
-        } 
+    if (comprobarCaracteres()){
+        for (let index = 0; index < auxVar.length; index++) {
+            if (valorInput == auxVar[index]){
+                aciertos++;
+                booleana = true;
+                document.getElementById("span"+index).innerHTML = valorInput;
+            }
+        }
     }
     //Comprobación para ver número de errores permitidos
     if (!booleana) {
@@ -85,10 +88,26 @@ function probar(){
     } else if (aciertos == auxVar.length){
         alert("Felicidades ha ganado el juego.");
         clearInterval(cont);
+        setTimeout(recargar, 5000);
     }
 
 }
 
+//Metodo para implementar el comprobador de caracteres
+function comprobarCaracteres(){
+    document.getElementById("caracteres").innerHTML = "Caracteres Introducidos:<br><br>"+caracteres;
+    if ((caracteres.indexOf(document.getElementById("texto").value.toUpperCase())) == -1){
+        caracteres[contadorCaracteres] = document.getElementById("texto").value.toUpperCase();
+        contadorCaracteres++;
+        document.getElementById("caracteres").innerHTML = "Caracteres Introducidos:<br><br>"+caracteres;
+        return true;
+    } else {
+        alert("Por favor pruebe con otro caracter que no haya introducido.");
+        return false;
+    }
+}
+
+//Funcion que recarga la página.
 function recargar(){
     location.reload();
 }
