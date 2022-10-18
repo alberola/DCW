e04c2ea9e1b6a1247ad299a24dd4a870a1b542c1
alberola <93872954+alberola@users.noreply.github.com>
2022-10-18 16:27:14 +0200

    actualización

--- AplicacionesDeportivas/script.js
+++ AplicacionesDeportivas/script.js
@@ -6,4 +6,22 @@ function hora(){
     document.getElementById("anio").innerHTML = fechaActual.getFullYear();
     document.getElementById("dia").innerHTML = fechaActual.getDate();
     document.getElementById("mes").innerHTML = fechaActual.toLocaleDateString('es-ES', {month: 'long'});
+} 
+
+function longitud(password){
+        return Boolean(password >= 8 && password <= 16);
 }
+document.getElementById('password').addEventListener('input', function(evt) {
+        
+        valido = document.getElementById('contrasenia');
+        
+        regex = /^/;
+
+    if (regex.test(password.value) && longitud(password.value)) {
+        valido.setAttribute("style", "color:green;","font-weight:bold;");
+        valido.innerHTML = "Contraseña válida.";
+    } else {
+        setInterval(valido.setAttribute("style", "color:red;","font-weight:bold;"), 1000);
+        valido.innerHTML = "Contraseña incorrecta.";
+    }
+  });
