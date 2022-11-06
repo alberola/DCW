033b03b6694bcdbe3c48be164af5591c31c3203c
alberola <93872954+alberola@users.noreply.github.com>
2022-11-06 02:03:33 +0100

    Edificios

--- 4.Edificios/edificios.js
+++ 4.Edificios/edificios.js
@@ -4,14 +4,9 @@ function Edificio(calle,numero,codigo){
     this.numero=numero;
     this.codigo=codigo;
     // El número de plantas y puertas puede variar pero no hacen falta
-    // indicarlo cuando se crea la clase
     this.numplantas=0;
     this.puertas=new Array();
     // Los propietarios tendrán asignado un piso de una planta, pero
-    // no tiene por qué indicarlos al crear la clase, aunque al igual que
-    // las plantas y puertas, se ha de inicializar su contenedor.
-    // Utilizo un array bidimensional para que el primer índice recoja el
-    // nº de la planta y el segundo el número del piso
     this.propietario=new Array();
     this.propietario[0]=new Array();
     // Indicamos cuáles serán los métodos que podrá gestionar la clase
@@ -25,36 +20,16 @@ function Edificio(calle,numero,codigo){
     this.imprimeCodigoPostal=imprimeCodigoPostal;
     this.imprimePlantas=imprimePlantas;
 
-    // Setters 
-    function setNumero(numero){
-        this.numero=numero;
-    }
-    function setCalle(calle){
-        this.calle=calle;
-    }
-    function setCodigoPostal(codigo){
-        this.codigo=codigo;
-    }
-    // Getters
-    function getCalle(){
-        return this.calle;
-    }
-    function getNumero(){
-        return this.numero;
-    }
-    function getCodigoPostal(){
-        return this.codigo;
-    }
-
-
-    // función para insertar número de plantas y puertas que tendrá el edificio
+    // Creamos el método encargado de indicarle a la clase el número de
     function agregarPlantasYPuertas(numplantas,puertas){
+        // Comprobamos que no hayamos introducido un número nulo o negativo de plantas
         if(numplantas<=0){
-            document.write("No puede agregar un numero nulo o negativo de plantas<br />");
+            errorText.innerHTML += ("No puede agregar un numero nulo o negativo de plantas<br />");
         // Si el número de plantas es correcto
         }else{
             // Almacenamos la longitud del primer índice del array 'propietario'
             var cuantos = this.propietario.length;
+            // Si hemos introducido un número de puertas por plantas, lo insertamos en cada una
             if(puertas>0){
                 for(var i=this.numplantas+1;i<=this.numplantas+numplantas;i++)
                     this.puertas[i]=puertas;
@@ -62,7 +37,7 @@ function Edificio(calle,numero,codigo){
                 for(var i=this.numplantas+1;i<=this.numplantas+numplantas;i++)
                     this.puertas[i]=this.puertas[this.numplantas];
                 
-                document.write("NO se pueden introducir número de puertas negativos o nulos.<br />Añadidas las mismas que las anteriores.<br />");
+                errorText.innerHTML += ("No se pueden introducir número de puertas negativos o nulos.<br />Añadidas las mismas que las anteriores.<br />");
             }
             // añadimos a las plantas existentes las que le pasamos al método como parámetro
             this.numplantas+=numplantas;
@@ -71,13 +46,11 @@ function Edificio(calle,numero,codigo){
                 this.propietario[i] = new Array();
                 // Las puertas de la planta creada se inicializan sin propietarios
                 for(var e=1;e<=this.puertas[i];e++)
-                    this.propietario[i][e]="VACÍO";
+                    this.propietario[i][e]= "";
             }
         }
     }
     // Método para agregar propietarios a cada una de las puertas
-    // Utilizo los parámetros planta y puerta como clave del índice para almacenar
-    // los nombres de los propietarios
     function agregarPropietario(nombre,planta,puerta){
         // Comprobamos si hemos introducido un número de planta erróneo
         if(planta>this.propietario.length-1 || planta<1){
@@ -85,34 +58,74 @@ function Edificio(calle,numero,codigo){
         // Comprobamos si hemos introducido un número de puerta erróneo
         }else if(puerta>this.propietario[planta].length-1 || puerta<1){
             textoError(nombre,puerta,planta);
-        // Si no se han producido errores
         }else{
             this.propietario[planta][puerta]=nombre;
             // Imprimimos el mensaje de propietario creado cada vez que se asigne
-            // un piso a un propietario
-            document.write(this.propietario[planta][puerta]+" es ahora el propietario de la puerta "+puerta+" de la planta "+planta+"<br />");
+            errorText.innerHTML += (this.propietario[planta][puerta]+" es ahora el propietario de la puerta "+puerta+" de la planta "+planta+"<br>");
         }
     }
-    
+    // Tres métodos que nos permitirá cambiar los datos del edificio
+    function modificarNumero(numero){
+        this.numero=numero;
+    }
+    function modificarCalle(calle){
+        this.calle=calle;
+    }
+    function modificarCodigoPostal(codigo){
+        this.codigo=codigo;
+    }
+    // Tres métodos que nos imprime los datos del edificio
+    function imprimeCalle(){
+        return this.calle;
+    }
+    function imprimeNumero(){
+        return this.numero;
+    }
+    function imprimeCodigoPostal(){
+        return this.codigo;
+    }
     // Función que recorre el array de propietarios mostrando todos sus datos.
-    // En this.numplantas tenemos el tamaño del array, y en this.puertas tenemos
-    // almacenado el número de pisos por planta. Los índices de 'this.propietario'
-    // coinciden con la planta y la puerta.
-    // Comenzaremos indicando la dirección del edificio al que pertenece
     function imprimePlantas(){
-        document.write("<b>Listado de propietarios del edificio calle "+this.imprimeCalle()+" nº: "+this.imprimeNumero()+"</b><br />");
+        errorText.innerHTML += ("<strong>Listado de propietarios del edificio calle "+this.imprimeCalle()+" nº: "+this.imprimeNumero()+"</strong><br>");
         for(var i=1;i<=this.numplantas;i++){
             for(var e=1;e<=this.puertas[i];e++){
-                document.write("El propietario del piso "+e+" de la planta "+i+" es: "+this.propietario[i][e]+"<br />");
+                errorText.innerHTML += ("El propietario del piso "+e+" de la planta "+i+" es: "+this.propietario[i][e]+"<br>");
             }
         }
     }
     // Método que recibe los parámetros del error que deseamos imprimir
     // No tenemos que declararlo al principio de la clase porque sólo se llamará desde su interior
     function textoError(nomb,nume,piso){
-            document.write("PROPIETARIO ("+nomb+") NO ASIGNADO <b>La puerta ("+nume+"ª) del piso ("+piso+"º) no existe</b><br />");
+        errorText.innerHTML += ("PROPIETARIO ("+nomb+") NO ASIGNADO <strong>La puerta ("+nume+"ª) del piso ("+piso+"º) no existe</strong><br>");
     }
     // Cada objeto creado con esta clase hará que nos muestre el mensaje de
     // Construido nuevo edificio en calle: tal nº: cual C.P.: sea
-    document.write("Construido nuevo edificio en calle: "+this.imprimeCalle()+" nº: "+this.imprimeNumero()+", C.P.: "+this.imprimeCodigoPostal()+"<br />");
+    errorText.innerHTML += ("Construido nuevo edificio en calle: "+this.imprimeCalle()+" nº: "+this.imprimeNumero()+", C.P.: "+this.imprimeCodigoPostal()+"<br>");
+}
+var errorText = document.getElementById("error-text");
+//Funcion que ejecuta pruebas para mostrar resultados al pulsar el boton
+function cargarPruebas(){
+    errorText.innerHTML = "";
+    //Insertamos 3 edificios e mprimimos datos de los mismos
+    var edificioA = new Edificio("García Prieto","58","15706");
+    var edificioB = new Edificio("Camino Caneiro","29","32004");
+    var edificioC = new Edificio("San Clemente","s/n","15705");
+    errorText.innerHTML += "El código postal del edificio A es: "+edificioA.imprimeCodigoPostal()+"<br>";
+    errorText.innerHTML += "La calle del edificio C es: "+edificioC.imprimeCalle()+"<br>";
+    errorText.innerHTML += "El edificio B está situado en la calle "+edificioB.imprimeCalle()+" número "+edificioB.imprimeNumero()+"<br>";
+    // Agregamos dos plantas con tres puertas cada una
+    edificioA.agregarPlantasYPuertas(2,3);
+    // Añadimos varios propietarios
+    edificioA.agregarPropietario("José Antonio López",1,1);
+    edificioA.agregarPropietario("Luisa Martínez",1,2);
+    edificioA.agregarPropietario("Marta Castellón",1,3);
+    edificioA.agregarPropietario("Antonio Pereira",2,2);
+    // Listamos los propietarios de cada una de las viviendas, comenzando con
+    edificioA.imprimePlantas();
+    // Agregamos una nueva planta con su número de puertas
+    edificioA.agregarPlantasYPuertas(1,1);
+    // Añadimos un nuevo propietario para la puerta 2 del piso 3º
+    edificioA.agregarPropietario("Pedro Meijide",2,3);
+    // Volvemos a imprimir un listado de propietarios, pero en esta ocasión tendrá un
+    edificioA.imprimePlantas();
 }
