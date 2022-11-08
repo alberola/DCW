5a30a5dfdf486420317c77ba6a3572b09954bc3d
alberola <93872954+alberola@users.noreply.github.com>
2022-11-08 16:24:15 +0100

    actualizacion

--- 5_Formulario/formulario.js
+++ 5_Formulario/formulario.js
@@ -1,18 +1,16 @@
-function obtenerDatosForm(){
-    const seleccionElemento = document.querySelector("form");
-    seleccionElemento.addEventListener("submit", e => {
-        e.preventDefault();
-        const data = Object.fromEntries(new FormData(e.target))
-        alert(JSON.stringify(data));
-    });
-}
 
-const asd = document.querySelectorAll("input");
+document.getElementById("nombre").addEventListener("input", function () {validar(this)});
+document.getElementById("nombre").addEventListener("blur", function () {document.getElementById("nombre").value = document.getElementById("nombre").value.toUpperCase();});
+document.getElementById("apellidos").addEventListener("input", function () {validar(this)});
+document.getElementById("apellidos").addEventListener("blur", function () {document.getElementById("apellidos").value = document.getElementById("apellidos").value.toUpperCase();});
+document.getElementById("edad").addEventListener("input", function () {validar(this)});
+document.getElementById("nif").addEventListener("input", function () {validar(this)});
 
-asd.addEventListener("input", function () {validar(parametro)});
+
 
 function validar(parametro){
-    let nombreInput = parametro.value;
+    let nombreInput = parametro.name;
+    let valorInput = parametro.value;
     //Parametro para validar nif
     let nif = /^\d{8}-[A-Z]$/;
     let email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -20,43 +18,66 @@ function validar(parametro){
     let hora = /\d+:\d:/;
     switch (nombreInput){
         case "nombre":
-            if (isNaN(nombreInput)){
-                document.getElementById("nombre").innerHTML = "Recuerda que el nombre tiene que ser una cadena."
-            }
+            let nombre = document.getElementById("nombreError");
+            if (valorInput == ""){
+                nombre.innerHTML = "";   
+            } else {
+                if (!isNaN(valorInput)){
+                    nombre.style.color = "red";
+                    nombre.innerHTML = "Recuerda que el nombre tiene que ser una cadena."
+                }  
+            }                     
             break;
         case "apellidos":
-            if (isNaN(nombreInput)){
-                document.getElementById("apellidos").innerHTML = "Recuerda que los apellidos tienen que ser una cadena."
+            let apellidos = document.getElementById("apellidosError");
+            if (valorInput == ""){
+                apellidos.innerHTML = "";
+            } else {
+                if (!isNaN(valorInput)){
+                    apellidos.style.color = "red";
+                    apellidos.innerHTML = "Recuerda que los apellidos tienen que ser una cadena."
+                }
             }
             break;
         case "edad":
-            if (!isNaN(nombreInput) || nombreInput > 0 || nombreInput < 105){
-                document.getElementById("edad").innerHTML = "La edad tiene que estar comprendida entre 1 y 104."
+            let edad = document.getElementById("edadError");
+            if (valorInput == ""){
+                edad.innerHTML = "";
+            } else {
+                if (isNaN(valorInput) || nombreInput < 0 || nombreInput > 105){
+                    edad.style.color = "red";
+                    edad.innerHTML = "La edad tiene que estar comprendida entre 1 y 104."
+                }
             }
             break;
         case "nif":
-            if (!nif.test(nombreInput)){
-                document.getElementById("nif").innerHTML = "Formato de nif incorrecto";
+            let nif = document.getElementById("nifError");
+            if (valorInput == ""){
+                nif.innerHTML = "";
+            } else {
+                if (!nif.test(valorInput)){
+                    document.getElementById("nifError").innerHTML = "Formato de nif incorrecto";
+                }
             }
             break;
         case "email":
-            if (!email.test(nombreInput)){
-                document.getElementById("email").innerHTML = "Formato de email incorrecto";
+            if (!email.test(valorInput)){
+                document.getElementById("emailError").innerHTML = "Formato de email incorrecto";
             }   
             break;
         case "provincia":
-                if (nombreInput == 0){
-                    document.getElementById("provincia").innerHTML = "Debe seleccionar una Provincia.";
+                if (valorInput == 0){
+                    document.getElementById("provinciaError").innerHTML = "Debe seleccionar una Provincia.";
                 }
             break;
         case "fecha":
-                if (!fecha.test(nombreInput)){
-                    document.getElementById("fecha").innerHTML = "Formato de fecha incorrecto";
+                if (!fecha.test(valorInput)){
+                    document.getElementById("fechaError").innerHTML = "Formato de fecha incorrecto";
                 }
             break;
         case "hora":
-                if (!hora.test(nombreInput)){
-                    document.getElementById("hora").innerHTML = "Formato de hora incorrecto";
+                if (!hora.test(valorInput)){
+                    document.getElementById("horaError").innerHTML = "Formato de hora incorrecto";
                 }
             break;
     }
