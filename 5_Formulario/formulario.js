0f617d2ae5724e0e4a03ba7d9055bd15854fbfa6
alberola <93872954+alberola@users.noreply.github.com>
2022-11-08 01:26:56 +0100

    i

---
+++ 5_Formulario/formulario.js
@@ -0,0 +1,6 @@
+const seleccionElemento = document.querySelector("form");
+seleccionElemento.addEventListener("submit", e => {
+   e.preventDefault();
+   const data = Object.fromEntries(new FormData(e.target))
+    alert(JSON.stringify(data));
+});
