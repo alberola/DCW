8e42066451b2fe269f1f96a3e613e8d0692c4a23
alberola <93872954+alberola@users.noreply.github.com>
2023-01-10 16:36:08 +0100

    apiRon

--- ApiRon/js/products.js
+++ ApiRon/js/products.js
@@ -1,14 +1,15 @@
 //Guardamos en una variable el div donde queremos introducir las cartas
 var respuesta = document.querySelector('#respuesta');
 
+//Var for the shop cart
+var cart = [];
+var objeto;
 //Funcion para obtener el idDrink (valor del mismo)
 function imprimirId(idDrink){
     console.log(idDrink.id);
-}
-//Función auxiliar para imprimirId
-function ayudaImprimir(){
-    console.log(this);
-    imprimirId(this);
+    cart.push(idDrink.id);
+    console.log(cart);
+    document.querySelector('.elementosCanvas').innerHTML += "<li class='list-group-item'>"+ idDrink.id + "</li>";
 }
 
     //Hacemos un llamamiento a la api y en caso de que entren los datos elegimos lo que deseamos mostrar
@@ -23,15 +24,25 @@ function ayudaImprimir(){
             data.drinks.forEach(element => {
                 respuesta.innerHTML += 
                 "<div class='col my-4'>"+
-                    "<div class='card' style='width: 18rem;'>"+
+                    "<div class='card' style='width: 18rem;' id="+element.idDrink+">"+
                         "<img src="+element.strDrinkThumb+" class='card-img-top' alt='...'>"+
                         "<div class='card-body'>"+
-                            "<h5 class='card-title m-b5'>"+element.strDrink+"</h5>"+
-                            "<a href='#' class='btn btn-dark boton' id="+element.idDrink+" onclick=ayudaImprimir()>Comprar</a>"+
+                            "<h5 id="+element.strDrink+" class='card-title m-b5'>"+element.strDrink+"</h5>"+
+                            "<a href='#' class='btn btn-dark boton' id="+element.idDrink+" data-bs-toggle='offcanvas' data-bs-target='#offcanvasWithBothOptions' aria-controls='offcanvasWithBothOptions' onclick=imprimirId(this)>Añadir al Carrito</a>"+
                         "</div>"+
                     "</div>"+
                 "</div>"; 
             });
         }
     })
+//Event Listener to clear the offCanvas cart
+document.querySelector('#clearCanvas').addEventListener("click", function clearCanvas() {
+    document.querySelector('.elementosCanvas').innerHTML = "";
+    cart = [];
+})
+
+
+
+
+
 
