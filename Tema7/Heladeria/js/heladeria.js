1e095d221735dbddd51fd681e5d4e8215d69887d
alberola <93872954+alberola@users.noreply.github.com>
2023-01-23 19:47:04 +0100

    Heladería Empezada

---
+++ Tema7/Heladeria/js/heladeria.js
@@ -0,0 +1,7 @@
+//Var for the ice cream creation
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
