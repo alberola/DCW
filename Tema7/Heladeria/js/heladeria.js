8ea0a28ec3d9873b4b0d76de3a376f9901e27647
alberola <93872954+alberola@users.noreply.github.com>
2023-01-24 20:00:26 +0100

    heladeria 50%

--- Tema7/Heladeria/js/heladeria.js
+++ Tema7/Heladeria/js/heladeria.js
@@ -8,6 +8,31 @@ let stocks = {
 
 //Auto-increment bar width
 var progressVar = document.querySelector('.progress-bar');
+//Var for imgs
+var selector = document.querySelectorAll('img');
+
+selector.forEach(element => {
+    element.addEventListener('click', function () { getValues(this)})
+});
+
+//Function to obtain value selected
+function getValues (element) {
+    console.log(element.id);
+    let content = document.querySelector('.fruits');
+    content.classList.add('change');
+    content.style.display = 'none';
+
+}
+
+
+//TIMEOUTS ANIMATIONS
 setTimeout(() => {
     progressVar.style.width = '25%';
 }, 1000);
+//We remove the entrance animation and then we put the rotation animation
+setTimeout(() => {
+    selector.forEach(element => {
+        element.classList.remove('bounce-in-top');
+        element.classList.add('rotation');
+    });
+}, 1500);
