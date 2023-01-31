981bb6a5947f25841aaaf81a67dd951b76569323
alberola <93872954+alberola@users.noreply.github.com>
2023-01-31 18:32:38 +0100

    Api Pelis

--- ApiProject/js/script.js
+++ ApiProject/js/script.js
@@ -1,25 +1,74 @@
+//Var to obtain the global container
+var container = document.querySelector('#globalContainer');
 
-getMovies('https://api.themoviedb.org/3/movie/popular?api_key=dda4633aacd800647ce023600f1aae38&language=es-ES&page=1');
 
-function getMovies(url) {
+//Calling the api to obtain the values
+obtainGenresMovies('https://api.themoviedb.org/3/genre/movie/list?api_key=dda4633aacd800647ce023600f1aae38&language=es-ES');
+obtainGenresTvSeries('https://api.themoviedb.org/3/genre/tv/list?api_key=dda4633aacd800647ce023600f1aae38&language=es-ES');
+
+getMovies('https://api.themoviedb.org/3/movie/popular?api_key=dda4633aacd800647ce023600f1aae38&language=es-ES&page=1', 'Popular');
+getMovies('https://api.themoviedb.org/3/trending/all/day?api_key=dda4633aacd800647ce023600f1aae38', 'Trending');
+getMovies('https://api.themoviedb.org/3/search/movie?api_key=dda4633aacd800647ce023600f1aae38&language=en-US&query=Animaci%C3%B3n&page=1&include_adult=false', 'Anime');
+getMovies('https://api.themoviedb.org/3/movie/upcoming?api_key=dda4633aacd800647ce023600f1aae38&language=es-ES&page=1', 'Upcoming Movies');
+
+//Function to obtain the list of movies-series
+function getMovies(url,tittle) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        showMovies(data);
+        showMovies(data, tittle);
     });
 }
 
-function showMovies(value) {
-    value = value.results;
-    value.forEach(element => {
-        document.getElementById('movies').innerHTML += "<div class='card col-4 my-3'> "+
-            "<img src='https://image.tmdb.org/t/p/w500"+element.poster_path+"' class='card-img-top rounded w-100' alt='...'>"+
-            "<div class='card-body'>"+
-            "<h5 class='card-title'>"+element.original_title+"</h5>"+
-            "<p class='card-text'>"+element.overview+"</p>"+
-            "<a href='#' class='btn btn-primary'>Go somewhere</a>"+
-                "</div>"
-            "</div>";
+//Function to obtain Genres and use the api to call it...
+function obtainGenresMovies(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
     });
+}
+//Function to obtain Genres and use the api to call it...
+function obtainGenresTvSeries(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+}
+
+//Function to print movies-series into the web page
+function showMovies(value, tittle) {
+    value = value.results;
+    createRow(tittle);
+    value.forEach(element => {
+        createImg(tittle,element.poster_path);
+    });
+}
+
+//Function to create the distinct sections tittle and containers of each section (popular, tendring, animes...)
+function createRow(tittle) {
+    const createH1 = document.createElement('h1');
+    createH1.innerText = tittle;
+    createH1.className = 'text-white';
+    const createRow = document.createElement('div');
+    const createImgContainer = document.createElement('div');
+    createImgContainer.className = 'col';
+    createImgContainer.id = tittle;
+    createRow.className = 'row';
+    //Introducing the elements into de global container
+    container.appendChild(createH1);
+    container.appendChild(createRow);
+    createRow.appendChild(createImgContainer);
+}
+
+//Function to introduce the elements img elements into the sections
+function createImg(tittle,imgRoute){
+    const createImg = document.createElement('img');
+    createImg.src = 'https://image.tmdb.org/t/p/w500'+imgRoute;
+    createImg.alt = 'IMG';
+    createImg.style.width = '10%';
+    //Introducing the img into the section container
+    document.getElementById(tittle).appendChild(createImg);
 }
